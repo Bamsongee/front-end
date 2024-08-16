fe64ec88daecad3dcdc392a00a7a9ce4f6b65586
Yunji-Yun <dbswlvkdlxld2022@naver.com>
2024-08-17 04:41:55 +0900

    basename 삭제

--- ohmae/src/index.js
+++ ohmae/src/index.js
@@ -8,7 +8,7 @@ import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <BrowserRouter basename="/ohmae">
+    <BrowserRouter>
         <CookiesProvider>
             <App />
         </CookiesProvider>
